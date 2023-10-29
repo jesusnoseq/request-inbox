6aa646ec653a108fc1bc1a1388ff4539a8393e1a
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-10-29 18:34:33 +0100

    feat(skeleton): add go project scruture

---
+++ api/cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jesusnoseq/request-inbox/pkg/config"
+	"github.com/jesusnoseq/request-inbox/pkg/handler"
+)
+
+func main() {
+	config.LoadConfig(config.API)
+	err := config.ConfigureLog()
+	if err != nil {
+		log.Fatal("configuring logssss", err)
+	}
+
+	r := gin.Default()
+
+	// Serve static files from the "static" folder
+	r.Static("/static", "./static")
+
+	// Define the routes and their handlers
+	v1 := r.Group("/api/v1")
+	{
+		inboxs := v1.Group("/inboxs")
+		{
+			inboxs.GET("/", handler.ListInboxs)
+			inboxs.POST("/", handler.CreateInbox)
+			inboxs.DELETE("/:id", handler.DeleteInbox)
+			inboxs.GET("/:id", handler.GetInbox)
+			inboxs.Any("/:id/in/", handler.InboxRequest)
+		}
+	}
+
+	// Serve the web page at the root path
+	r.LoadHTMLGlob("../static/*.html")
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+	})
+
+	err = r.Run(":" + config.GetString(config.APIHTTPPort))
+	if err != nil {
+		log.Fatal("server was closed", err)
+	}
+}
---
+++ api/pkg/handler/handlers.go
@@ -0,0 +1,75 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/jesusnoseq/request-inbox/pkg/model"
+)
+
+var inboxes []model.Inbox
+
+func ListInboxs(c *gin.Context) {
+	c.JSON(http.StatusOK, inboxes)
+}
+
+func CreateInbox(c *gin.Context) {
+	// Parse request body and create a new inbox
+	var newInbox model.Inbox
+	if err := c.ShouldBindJSON(&newInbox); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	newInbox.ID = uuid.New()
+	inboxes = append(inboxes, newInbox)
+
+	c.JSON(http.StatusCreated, newInbox)
+}
+
+func DeleteInbox(c *gin.Context) {
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid inbox ID"})
+		return
+	}
+
+	// Find and delete the inbox with the given ID
+	for i, inbox := range inboxes {
+		if inbox.ID == id {
+			inboxes = append(inboxes[:i], inboxes[i+1:]...)
+			c.JSON(http.StatusNoContent, nil)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "Inbox not found"})
+}
+
+func GetInbox(c *gin.Context) {
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid inbox ID"})
+		return
+	}
+
+	// Find and return the inbox with the given ID
+	for _, inbox := range inboxes {
+		if inbox.ID == id {
+			c.JSON(http.StatusOK, inbox)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"error": "Inbox not found"})
+}
+
+func InboxRequest(c *gin.Context) {
+	// Implement the handling of requests associated with an inbox
+	// This endpoint should capture requests and associate them with the inbox.
+	// You can store these requests in the corresponding inbox's "Requests" field.
+	// Make sure to handle different HTTP methods and paths as needed.
+
+	c.JSON(http.StatusNotImplemented, gin.H{"error": "Not implemented"})
+}
