c307f1a65be69c18ba102e88ed227d908f63e703
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-10-31 00:23:08 +0100

    feat(dao.go): add DAO interface

---
+++ api/pkg/database/dao.go
@@ -0,0 +1,17 @@
+package database
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/jesusnoseq/request-inbox/pkg/model"
+)
+
+type InboxDAO interface {
+	CreateInbox(context.Context, model.Inbox) (model.Inbox, error)
+	UpdateInbox(context.Context, model.Inbox) (model.Inbox, error)
+	GetInbox(context.Context, uuid.UUID) (model.Inbox, error)
+	DeleteInbox(context.Context, uuid.UUID) error
+	ListInbox(context.Context) ([]model.Inbox, error)
+	Close(context.Context) error
+}
