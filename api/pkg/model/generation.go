9d38e4ee81dfa3a0bf014046786bc2986d33c9c8
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-11-01 12:23:33 +0100

    refactor(model): move model generation functions

---
+++ api/pkg/model/generation.go
@@ -0,0 +1,40 @@
+package model
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"math"
+	"time"
+)
+
+// Generates a Base16 random string
+func randomString(l int) string {
+	buff := make([]byte, int(math.Ceil(float64(l)/2)))
+	rand.Read(buff)
+	str := hex.EncodeToString(buff)
+	return str[:l] // strip 1 extra character we get from odd length results
+}
+
+func GenerateInbox() Inbox {
+	return Inbox{
+		Timestamp: time.Now().UnixMilli(),
+		Response: Response{
+			ResponseBody:    "response body" + randomString(5),
+			ResponseHeaders: map[string]string{randomString(5): randomString(5)},
+		},
+		Requests:              []Request{GenerateRequest(1), GenerateRequest(2)},
+		ObfuscateHeaderFields: []string{"Authorization"},
+	}
+}
+
+func GenerateRequest(id int) Request {
+	return Request{
+		ID:        id,
+		Timestamp: time.Now().UnixMilli(),
+		Headers: map[string]string{
+			"Content-Type":  "application/json",
+			"Authorization": "Bearer access_token" + randomString(5),
+		},
+		Body: "This is the request body." + randomString(10),
+	}
+}
