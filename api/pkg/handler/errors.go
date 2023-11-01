f4971d40234fa0ddbfddd36cc9ca6029f67b5ae3
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-11-01 16:18:14 +0100

    feat(handlers): add error handlers

---
+++ api/pkg/handler/errors.go
@@ -0,0 +1,29 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jesusnoseq/request-inbox/pkg/model"
+)
+
+const (
+	notFoundMessage         = "resource not found"
+	methodNotAllowedMessage = "method not allowed"
+)
+
+func MethodNotAllowedHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusMethodNotAllowed,
+		model.ErrorResponse{
+			Code:    http.StatusMethodNotAllowed,
+			Message: methodNotAllowedMessage,
+		})
+}
+
+func NotFoundHandler(ctx *gin.Context) {
+	ctx.JSON(http.StatusNotFound,
+		model.ErrorResponse{
+			Code:    http.StatusNotFound,
+			Message: notFoundMessage,
+		})
+}
