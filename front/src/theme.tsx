b7f68da2b35e81daf241929bac1deb4a36636df7
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-12-03 01:28:33 +0100

    feat(front): allows to save theme pref

--- front/src/theme.tsx
+++ front/src/theme.tsx
@@ -3,14 +3,27 @@ import CssBaseline from '@mui/material/CssBaseline';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import React, { createContext, useContext, useMemo, useState, ReactNode } from 'react';
 
+const saveThemePreference = (themeName: string) => {
+    localStorage.setItem('themePreference', themeName);
+};
+
+
+const loadThemePreference = () => {
+    const theme = localStorage.getItem('themePreference');
+    return theme
+};
+
 export const ThemeContext = createContext({
     toggleTheme: () => { },
     isDarkMode: false,
 });
 
+
+
 export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-    const [isDarkMode, setDarkMode] = useState(prefersDarkMode);
+    const savedTheme = loadThemePreference()
+    const [isDarkMode, setDarkMode] = useState((savedTheme != null && savedTheme === 'dark') || (prefersDarkMode && savedTheme === null));
 
     const theme = useMemo(
         () =>
@@ -24,6 +37,7 @@ export const CustomThemeProvider = ({ children }: { children: ReactNode }) => {
 
     const toggleTheme = () => {
         setDarkMode(!isDarkMode);
+        saveThemePreference(!isDarkMode ? 'dark' : 'light');
     };
 
     return (
