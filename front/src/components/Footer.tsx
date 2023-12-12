c8cf39a04b88726843a5fcea1189a7c6e67e09c9
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-12-12 14:55:05 +0100

    feat(front): improve links (#16)

--- front/src/components/Footer.tsx
+++ front/src/components/Footer.tsx
@@ -1,11 +1,15 @@
 import { Typography, Container, Box } from '@mui/material';
+import ExternalLink from '../components/ExternalLink'
 
 export default function Footer() {
     return (
         <footer>
             <Box component="footer" bgcolor="background.paper" py={2} textAlign="center">
                 <Container maxWidth="sm">
-                    <Typography>© 2023 Jesusnoseq. Licensed under the <a href='https://github.com/jesusnoseq/request-inbox/blob/main/LICENSE'>Apache License, Version 2.0</a>.</Typography>
+                    <Typography>© 2023&nbsp;
+                        <ExternalLink href="https://jesusnoseq.com/">Jesusnoseq</ExternalLink>.
+                        Licensed under the <ExternalLink href='https://github.com/jesusnoseq/request-inbox/blob/main/LICENSE'>Apache License, Version 2.0</ExternalLink>.
+                    </Typography>
                 </Container>
             </Box>
         </footer>
