c39ec85fd85e5914ad3daedb02d8b43e3e80fd34
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-12-17 14:18:13 +0100

    feat(front): add api doc page (#21)
    * feat(front): add api doc page
    
    * chore(front): fix import warning

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InboxListPage from './pages/InboxListPage';
 import InboxDetailPage from './pages/InboxDetailPage';
 import AboutPage from './pages/AboutPage';
+import APIDocPage from './pages/APIDocPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<InboxListPage />} />
         <Route path="/inbox" element={<InboxListPage />} />
+        <Route path="/api-doc" element={<APIDocPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/inbox/:inboxId" element={<InboxDetailPage />} />
       </Routes>
