f724fa058ed2db24282e1e647b6e54abef6decbe
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2023-12-27 12:35:45 +0100

    feat(front):  small ui improvements (#25)

--- front/src/components/HeadersEditor.tsx
+++ front/src/components/HeadersEditor.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, IconButton, List, ListItem, ListItemText, ListItemSecondaryAction, Stack } from '@mui/material';
+import { TextField, Button, IconButton, List, ListItem, ListItemText, ListItemSecondaryAction, Stack, Autocomplete } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import DeleteIcon from '@mui/icons-material/Delete';
 
@@ -27,6 +27,30 @@ interface HeadersEditorProps {
     onHeadersChange: (headers: Header[]) => void;
 }
 
+const standardResponseHeaders = [
+    "Access-Control-Allow-Origin",
+    "Access-Control-Expose-Headers",
+    "Age",
+    "Cache-Control",
+    "Content-Disposition",
+    "Content-Encoding",
+    "Content-Language",
+    "Content-Length",
+    "Content-Type",
+    "Date",
+    "ETag",
+    "Expires",
+    "Last-Modified",
+    "Location",
+    "Pragma",
+    "Retry-After",
+    "Server",
+    "Set-Cookie",
+    "Transfer-Encoding",
+    "Vary",
+    "WWW-Authenticate",
+    "X-Frame-Options"
+]
 
 
 const HeadersEditor: React.FC<HeadersEditorProps> = ({ initialHeaders, onHeadersChange }) => {
@@ -72,13 +96,18 @@ const HeadersEditor: React.FC<HeadersEditorProps> = ({ initialHeaders, onHeaders
                 ))}
             </List>
             <Stack direction="row" spacing={2}>
-                <TextField
-                    label="Key"
-                    value={newHeader.key}
-                    onChange={handleKeyChange}
-                    margin="normal"
-                    variant="outlined"
+                <Autocomplete
+                    id="free-solo-demo"
+                    freeSolo
+                    options={standardResponseHeaders}
                     sx={{ flex: 0.5 }}
+                    value={newHeader.key}
+                    disableClearable
+                    renderInput={(params) => <TextField
+                        {...params}
+                        label="Key"
+                        onChange={handleKeyChange}
+                        variant="outlined" />}
                 />
                 <TextField
                     label="Value"
