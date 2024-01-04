1463b23eebb30cefb85ddccff607d7fda66b749e
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-01-04 17:11:02 +0100

    feat(front):  add health page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -4,6 +4,8 @@ import InboxListPage from './pages/InboxListPage';
 import InboxDetailPage from './pages/InboxDetailPage';
 import AboutPage from './pages/AboutPage';
 import APIDocPage from './pages/APIDocPage';
+import HealthPage from './pages/HealthPage';
+
 
 function App() {
   return (
@@ -14,6 +16,8 @@ function App() {
         <Route path="/api-doc" element={<APIDocPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/inbox/:inboxId" element={<InboxDetailPage />} />
+        <Route path="/health" element={<HealthPage />} />
+
       </Routes>
     </Router>
   );
