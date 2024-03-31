d859506c0f263d33d920bcf3bb2171843e699ce1
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-03-31 18:08:05 +0200

    refactor(api): split health from handlers

---
+++ api/pkg/handler/health_test.go
@@ -0,0 +1,39 @@
+package handler_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jesusnoseq/request-inbox/pkg/config"
+)
+
+func TestInboxHealth(t *testing.T) {
+	config.LoadConfig(config.Test)
+	config.Set(config.DBEngine, config.DBEngineBadger)
+	ih, closer := mustGetInboxHandler()
+	defer closer()
+
+	w := httptest.NewRecorder()
+	ginCtx, _ := gin.CreateTestContext(w)
+	req, err := http.NewRequest(
+		"GET",
+		"",
+		nil,
+	)
+	if err != nil {
+		t.Error(err)
+	}
+	ginCtx.Request = req
+	ih.Health(ginCtx)
+	resp := w.Result()
+	resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		t.Errorf("Expected StatusOK, got %v", resp.StatusCode)
+	}
+	want := `{"embededDB":true,"snapshot":"local","status":"pass","version":"0.1"}`
+	if w.Body.String() != want {
+		t.Errorf("Expected response %q, got %q", want, w.Body.String())
+	}
+}
