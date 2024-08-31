309ba26009aa8d6c8668c2bf3acaf0e65173451e
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-08-31 12:41:22 +0200

    feat(front): scroll consistency to avoid page vibration

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Container from '@mui/material/Container';
+
+import AboutPage from './pages/AboutPage';
 import InboxListPage from './pages/InboxListPage';
 import InboxDetailPage from './pages/InboxDetailPage';
-import AboutPage from './pages/AboutPage';
 import APIDocPage from './pages/APIDocPage';
 import HealthPage from './pages/HealthPage';
 import UsersManualPage from './pages/UsersManualPage';
-
+import UserProfilePage from './pages/UserProfilePage';
+import ScrollConsistencyLayout from './components/ScrollConsistencyLayout';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<InboxListPage />} />
-        <Route path="/inbox" element={<InboxListPage />} />
-        <Route path="/api-doc" element={<APIDocPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/users-manual" element={<UsersManualPage />} />
-        <Route path="/inbox/:inboxId" element={<InboxDetailPage />} />
-        <Route path="/health" element={<HealthPage />} />
-
-      </Routes>
-    </Router>
+    <ScrollConsistencyLayout>
+      <Router>
+        <Container>
+          <Header />
+          <Routes>
+            <Route path="/" element={<InboxListPage />} />
+            <Route path="/inbox" element={<InboxListPage />} />
+            <Route path="/api-doc" element={<APIDocPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/users-manual" element={<UsersManualPage />} />
+            <Route path="/inbox/:inboxId" element={<InboxDetailPage />} />
+            <Route path="/profile" element={<UserProfilePage />} />
+            <Route path="/health" element={<HealthPage />} />
+          </Routes>
+          <Footer />
+        </Container>
+      </Router>
+    </ScrollConsistencyLayout>
   );
 }
 
