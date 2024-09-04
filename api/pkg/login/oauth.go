54c9f5a5720e9e6f56500db6c3f201a6a5fe7c92
Jesús Rodríguez Pérez <jesusnoseq@hotmail.com>
2024-09-05 00:50:27 +0200

    feat(back): fix linter issues

--- api/pkg/login/oauth.go
+++ api/pkg/login/oauth.go
@@ -3,6 +3,8 @@ package login
 import (
 	"crypto/rand"
 	"encoding/base64"
+
+	"golang.org/x/exp/slog"
 )
 
 const OauthStateCookieName = "oauthstate"
@@ -10,6 +12,9 @@ const AuthTokenCookieName = "auth_token"
 
 func generateStateString() string {
 	b := make([]byte, 16)
-	rand.Read(b)
+	_, err := rand.Read(b)
+	if err != nil {
+		slog.Error("Error generating string for oauth", "error", err)
+	}
 	return base64.URLEncoding.EncodeToString(b)
 }
