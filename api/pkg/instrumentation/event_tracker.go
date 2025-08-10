28e6c1d9567f37f413130d5015baf1f7c795e6da
jesusnoseq <jesusnoseq@hotmail.com>
2025-08-10 16:27:33 +0200

    feat(back): rename EventTracker interface and generate mock

--- api/pkg/instrumentation/event_tracker.go
+++ api/pkg/instrumentation/event_tracker.go
@@ -4,6 +4,8 @@ import (
 	"context"
 )
 
+//go:generate mockgen -source=event_tracker.go -destination=event_tracker_mock/event_tracker_mock.go -package=instrumentation_mock EventTracker
+
 type EventTracker interface {
 	Track(ctx context.Context, event TrackedEvent) error
 	Close() error
